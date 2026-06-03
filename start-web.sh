758ab5c78f3ba80920165cb948a5ac9f04bffcb9
karkyon <your-email@example.com>
2026-06-03 11:39:54 +0900

    fix: start-web.sh source -> . for pm2 compatibility

--- start-web.sh
+++ start-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
+. "$NVM_DIR/nvm.sh"
 cd /home/karkyon/projects/machcore/apps/web
 exec node_modules/.bin/next start -p 3010 -H 0.0.0.0
